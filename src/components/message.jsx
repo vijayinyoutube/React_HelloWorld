d0ace711a45d23ff34f718f044bfa92bcc99c874
Vijay <vijaythevirtue@gmail.com>
2020-07-20 17:20:54 +0530

    state change props+icons

---
+++ src/components/message.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { FaBell } from "react-icons/fa";
+
+class Message extends Component {
+  state = {
+    message: "Welcome Visitor",
+  };
+
+  handleSubscription = () => {
+    this.setState({
+      message: "Thanks for Subscribing!",
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h3>
+          {this.state.message} {this.props.children}
+        </h3>
+
+        <button
+          onClick={() => this.handleSubscription()}
+          type="button"
+          className="btn btn-outline-danger"
+        >
+          {" "}
+          <span>
+            <FaBell size="18.5"></FaBell>
+          </span>
+          <span>&nbsp;</span>
+          Subscribe
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Message;
