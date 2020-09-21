9ed759a4a628070855e90f62ebfbcad58b9a52fc
Vijay <vijaythevirtue@gmail.com>
2020-09-21 19:58:33 +0530

    event handlers 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,7 @@ export class Form extends Component {
 
         this.state = {
             username: "",
+            topic: "",
         }
     }
 
@@ -14,12 +15,37 @@ export class Form extends Component {
             username: event.target.value
         })
     }
+
+    handleTopicChangeEvent = (event) => {
+        this.setState({
+            topic: event.target.value,
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert(`${this.state.username} ${this.state.topic}`);
+        event.preventDefault(); //  <----|
+        // To avoid the entered data being lost when the page refresh after closing that alert
+        // box
+    }
     render() {
+        const { username, topic } = this.state;
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <label>Username</label>
-                    <input type="text" value={this.state.username} onChange={this.handleChangeEvent} />
+                    <input type="text" value={username} onChange={this.handleChangeEvent} />
+                </div>
+                <div>
+                    <label>Topic</label>
+                    <select value={topic} onChange={this.handleTopicChangeEvent}>
+                        <option>React</option>
+                        <option>Angular</option>
+                        <option>Vue</option>
+                    </select>
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
                 </div>
             </form>
         )
